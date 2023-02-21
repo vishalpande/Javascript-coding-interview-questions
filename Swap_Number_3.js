a0cd77558e52501486f30f69362b59749261031b
vishalpande <pandevishal40@gmail.com>
2023-02-21 20:07:57 +0530

    Swap Added

---
+++ Swap_Number_3.js
@@ -0,0 +1,26 @@
+// Given two number a and b return number after swap value to each other.
+
+// Example 1:
+// Input:
+// 5 8
+
+// Output:
+// { a: 8, b: 5 }
+
+function swap(){
+    let a=5;
+let b=6;
+let c=0; 
+
+c=a;
+a=b;
+b=c;
+
+console.log(a,b);
+
+}
+swap();
+
+
+
+
