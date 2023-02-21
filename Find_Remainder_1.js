54eb7c47866b4c0e8307c42c1544393195efa740
vishalpande <pandevishal40@gmail.com>
2023-02-21 18:54:35 +0530

    first commit

---
+++ Find_Remainder_1.js
@@ -0,0 +1,17 @@
+// Given two numbers a and b. Return the remainder when a is divided by b.
+
+// Example 1:
+// Input:
+// 5 8
+
+// Output:
+// 5
+
+// Explanation : -
+
+function findRemainder(a, b) {
+    return a%b;
+}
+    var a = 5;
+    var b =8;
+    console.log(findRemainder(a, b));
