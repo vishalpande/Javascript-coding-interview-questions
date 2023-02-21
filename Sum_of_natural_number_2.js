aa36b788718d370bbf005922a85118d34367addd
vishalpande <pandevishal40@gmail.com>
2023-02-21 19:28:44 +0530

    Adding sum of natural number

---
+++ Sum_of_natural_number_2.js
@@ -0,0 +1,18 @@
+// Given an interger n return the sum of all the natural numbers from 1 to n.
+
+// Example :
+// Input:
+// 5
+
+// Output:
+// 15
+Explinations:
+
+var a = 5;
+
+let sum=0;
+
+for (let i=0;i<=a;i++){
+    sum+=i;
+}
+console.log(sum);
