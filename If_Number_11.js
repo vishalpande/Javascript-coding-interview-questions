ce37dbe08130d2069b4439cccb960adcdd975892
vishalpande <pandevishal40@gmail.com>
2023-03-01 10:26:23 +0530

    Adding the progrram

--- If_Number_11.js
+++ If_Number_11.js
@@ -1,8 +1,16 @@
 // Write a JavaScript program that checks if input variable is a number by using isNaN() in-built JavaScript function.
-let a=2;
-if(a %2 ==0){
-    console.log("true");
-}else{
-    console.log("false")
-}
+
   
+function ValidNumber(num) {
+    if(isNaN(num)){
+      return false;
+    }else {
+     return true;
+    }
+  }
+  
+  function main() {
+      var a = 6;
+      
+      console.log(ValidNumber(a));
+  }
