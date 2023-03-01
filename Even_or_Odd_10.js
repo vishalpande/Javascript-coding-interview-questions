19b2f021a382afc5df75a1a9a593f2975f3738de
vishalpande <pandevishal40@gmail.com>
2023-03-01 10:18:56 +0530

    Adding the program

---
+++ Even_or_Odd_10.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program that checks if input number is even or odd by using “%” operator in JavaScript.
+let a=9;
+
+if(a %2 ==0){
+    console.log("true");
+}else{
+    console.log("false")
+}
+  
