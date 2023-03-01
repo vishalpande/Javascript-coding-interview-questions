d5c76a7f45dcfb062f4ac34a55aace0ac673eff1
vishalpande <pandevishal40@gmail.com>
2023-03-01 10:16:09 +0530

    adding the program

---
+++ cinditional_statements_9.js
@@ -0,0 +1,12 @@
+// Convert Following code into ternary
+
+let age = 15;
+let result;
+
+if (age >= 18) {
+      result = "You are eligible to vote.";
+} else {
+      result = "You are not eligible to vote yet.";
+}
+
+console.log(result);
