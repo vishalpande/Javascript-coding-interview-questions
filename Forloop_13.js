2b782fe9557dbfb1848e0be871df7430f553880b
vishalpande <pandevishal40@gmail.com>
2023-03-02 20:23:12 +0530

    Adding the program

---
+++ Forloop_13.js
@@ -0,0 +1,8 @@
+// How does the continue directive (statement) work in a loop? Does it stop the whole loop?
+
+for (let i = 0; i < 10; i++) {
+  // if true, skip the remaining part of the body
+  if (i % 2 == 0) continue;
+
+  alert(i); // 1, then 3, 5, 7, 9
+}
