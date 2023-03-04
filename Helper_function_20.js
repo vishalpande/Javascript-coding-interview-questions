25d0af772f0484db750261dd0d0b638271cc22cf
vishalpande <pandevishal40@gmail.com>
2023-03-04 23:25:13 +0530

    Adding a program

--- Helper_function_20.js
+++ Helper_function_20.js
@@ -1,2 +1,27 @@
 // we can write a helper function
 
+// const help=(a)=>{
+
+// return a+20;
+
+// }
+// const help2=(b)=>{
+// return help(b) +40;
+
+// }
+
+// console.log(help(3))
+
+
+
+
+function multiplyByNineFifths(number) {
+    return number * (9/5);
+  };
+   
+  function getFahrenheit(celsius) {
+    return multiplyByNineFifths(celsius) + 32;
+  };
+   
+  console.log(getFahrenheit(15)); // Returns 59
+  
