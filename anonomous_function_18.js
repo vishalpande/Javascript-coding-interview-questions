c56d386a1c6b57386e626400830b221d32ae985b
vishalpande <pandevishal40@gmail.com>
2023-03-04 22:51:20 +0530

    Adding the program

---
+++ anonomous_function_18.js
@@ -0,0 +1,12 @@
+// this is an anonomous function
+
+
+
+let task=function(task1,task2){
+
+console.log(`i will first complete  ${task1} then i will start ${task2}`)
+
+
+}
+
+task("reading","writing");
