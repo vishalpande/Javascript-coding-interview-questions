9bc80c2d0abdf109b7dda853aa61fa229223c740
vishalpande <pandevishal40@gmail.com>
2023-03-04 22:33:16 +0530

    Program is added

---
+++ avascript_Promise_16.js
@@ -0,0 +1,21 @@
+// Given two number , write a Javascript program that resolves a promise if a number is more than 42 else rejects it.
+
+function test(arr){
+    arr.forEach(function(element){
+		element.then(function(value){
+			if(value > 42){ 
+			    console.log(value + " is resolved")
+			}else {
+			   console.log(value+" is rejected");
+			}
+		})
+	})
+}
+function main() {
+    let num1=parseInt(readLine());
+    let num2=parseInt(readLine());
+	const n1 = Promise.resolve(num1)
+	const n2 = Promise.resolve(num2)
+	test([n1, n2])
+
+}
