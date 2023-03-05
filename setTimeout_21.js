404f57ce216571d3402718ad16b17faab788354d
vishalpande <pandevishal40@gmail.com>
2023-03-05 22:50:09 +0530

    Adding some problems

---
+++ setTimeout_21.js
@@ -0,0 +1,8 @@
+
+// program to display a text using setTimeout method
+function greet() {
+    console.log('Hello world');
+}
+
+setTimeout(greet, 3000);
+console.log('This message is shown first');
