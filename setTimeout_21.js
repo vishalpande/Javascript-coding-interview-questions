0fb38ceab372ad369b53bee3b82d7cecda4a0f3d
vishalpande <pandevishal40@gmail.com>
2023-03-05 22:55:35 +0530

    Updated

--- setTimeout_21.js
+++ setTimeout_21.js
@@ -5,4 +5,17 @@ function greet() {
 }
 
 setTimeout(greet, 3000);
-console.log('This message is shown first');
+console.log('This message is shown first');\\
+
+
+// console.log("first")
+
+// setTimeout(()=>{
+//     task("third")
+// },5000);
+
+// function task(message){
+//     console.log(message);
+// }
+
+// console.log("second")
