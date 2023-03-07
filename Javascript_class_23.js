9aa8b3cd4cd1c7cd81b002639922d76ea395ae64
vishalpande <pandevishal40@gmail.com>
2023-03-07 20:31:58 +0530

    Update

--- Javascript_class_23.js
+++ Javascript_class_23.js
@@ -1,3 +1,8 @@
+// Create a JavaScript class called Animals which accepts 2 input parameters 
+// (name and specie) as arguments and stores them internally. Now create a proto function 
+// called sing that prints the name of the animal with 'can sing' returned at the end.
+
+
 class Animals {
     constructor(name, specie){
     this.name = name;
