6e43774b5cb4098bf5be7e8d111a8e3372f0c6ae
vishalpande <pandevishal40@gmail.com>
2023-03-09 19:11:25 +0530

    Adding a program

---
+++ Chase on the Number Line_26.js
@@ -0,0 +1,9 @@
+// A painter discovered that one of his painting has been stolen. He immediately informs the police of the theft.
+
+// It is known that the policeman and thief move on the number line.
+
+// You are given that:
+
+// The initial location of the policeman on the number line is X and his speed is 2 units per second. The initial location of the thief on the number line is Y and his speed is 1 unit per second.
+
+// Find the minimum time (in seconds) in which the policeman can catch the thief. Note that, the policeman catches the thief as soon as their locations become equal and the thief will try to evade the policeman for as long as possible.
