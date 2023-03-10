c2feb0fe836720715b9c6170442a8cac2d36eef6
vishalpande <pandevishal40@gmail.com>
2023-03-10 21:30:50 +0530

    updaing

---
+++ bind_28.js
@@ -0,0 +1,19 @@
+// This method returns a new function, where the value of “this” keyword will be bound to the owner object, which is provided as a parameter.
+
+
+
+var bikeDetails = {
+    displayDetails: function(registrationNumber,brandName){
+    return this.name+ " , "+ "bike details: "+ registrationNumber + " , " + brandName;
+  }
+}
+   
+var person1 = {name:  "Vivek"};
+     
+var detailsOfPerson1 = bikeDetails.displayDetails.bind(person1, "TS0122", "Bullet");
+      
+// Binds the displayDetails function to the person1 object
+        
+      
+detailsOfPerson1();
+// Returns Vivek, bike details: TS0452, Thunderbird
