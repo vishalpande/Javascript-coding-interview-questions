96bc32b78d866c41e062cc9e33d6faf9881d6edc
vishalpande <pandevishal40@gmail.com>
2023-03-11 21:15:06 +0530

    Updating

--- Javascript Object_32.js
+++ Javascript Object_32.js
@@ -1,1 +1,11 @@
 // Given a string like an object , convert it into an object having 2 properties fname and lname .Return a string having fname and lname with space seperated using that object.
+
+
+function fullname(obj){
+    return obj.fname+" "+obj.lname;
+  }
+  function main() {
+      let str=readLine();
+      let user=JSON.parse(str)
+      console.log(fullname(user));
+  }
