5388e7c010ac236b726d63011dba7c0051553565
vishalpande <pandevishal40@gmail.com>
2023-03-11 21:03:58 +0530

    Adding a program

--- Javascript_Function _Constructor_31.js
+++ Javascript_Function _Constructor_31.js
@@ -1,2 +1,16 @@
 // Create a JavaScript constructor function called Animals which accepts 2 input parameters (name and specie) as arguments and stores them internally. Now create a proto function called details that prints the name of the animal and species as shown in the exmple.
 
+function Animals(name, specie) {
+    this.name = name;
+    this.specie = specie;
+}
+Animals.prototype.details = function(){
+    console.log(`Animal is ${this.name} and species is ${this.specie}`);
+}
+
+function main() {
+    let animal=readLine();
+    let species=readLine();
+    const obj = new Animals(animal, species);
+    obj.details();
+}
