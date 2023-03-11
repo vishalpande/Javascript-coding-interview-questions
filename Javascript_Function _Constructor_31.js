5171f766e7a16d3d180b9469f51ae4e3781a51a5
vishalpande <pandevishal40@gmail.com>
2023-03-11 21:02:49 +0530

    Updating

---
+++ Javascript_Function _Constructor_31.js
@@ -0,0 +1,2 @@
+// Create a JavaScript constructor function called Animals which accepts 2 input parameters (name and specie) as arguments and stores them internally. Now create a proto function called details that prints the name of the animal and species as shown in the exmple.
+
